7689ca6e3ec67142775932f64096fe97c18a5003
YooLc <dlulc@sina.com>
2020-03-16 21:21:53 +0800

    Initial Commit

---
+++ RandotronWPF/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace RandotronWPF
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const int MAXS = 1000;
+        private int totStudent = 0, current = 0;
+        private int[] studentNumList = new int[MAXS];
+        public MainWindow()
+        {
+            InitializeComponent();
+            clearAndShow();
+        }
+        public int GetStudentNumber(TextBox textBox)
+        {
+            string content = textBox.Text;
+            int t = 0;
+            for (int i = 0; i < content.Length; ++i)
+            {
+                if (content[i] >= '0' && content[i] <= '9')
+                    t = (t * 10) + content[i] - '0';
+                if (t > MAXS) return t;
+            }
+            return t;
+        }
+        private void SnackbarConfirmed(object sender, RoutedEventArgs e)
+        {
+            SnackbarTwo.IsActive = false;
+        }
+        private void popUp(string Message)
+        {
+            SnackbarTwoMessage.Content = Message;
+            SnackbarTwo.IsActive = true;
+        }
+        private void numberModeConfirmed(object sender, RoutedEventArgs e)
+        {
+            int number = GetStudentNumber(numberTextBox);
+            if (number <= 0) { popUp("请输入正确的学生人数，范围为[1, 1000]"); return; }
+            if (number > MAXS) { popUp("最多仅允许 1000 个学生"); return; }
+            numberTextBox.Text = number.ToString();
+            totStudent = number;
+            clearAndShow();
+            preText.Content = current > 0 ? studentNumList[current - 1].ToString() : "";
+            curText.Content = studentNumList[current].ToString();
+            nxtText.Content = current + 1 < totStudent ? studentNumList[current + 1].ToString() : "";
+            virtualTextBox.Focus();
+        }
+        private void random_Shuffle()
+        {
+            int currentIndex;
+            int tempValue;
+            Random r = new Random();
+            for (int i = totStudent - 1; i >= 0; i--)
+            {
+                currentIndex = r.Next(i + 1);
+                tempValue = studentNumList[currentIndex];
+                studentNumList[currentIndex] = studentNumList[i];
+                studentNumList[i] = tempValue;
+            }
+        }
+        private void clearAndShow()
+        {
+            ModeSelect.SelectedIndex = 0;
+            for (int i = 0; i < MAXS; ++i) studentNumList[i] = i + 1;
+            random_Shuffle();
+            current = 0;
+            ProgressBar.Value = 0;
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                current += 1;
+                if (current == totStudent) clearAndShow();
+                preText.Content = current > 0 ? studentNumList[current - 1].ToString() : "";
+                curText.Content = studentNumList[current].ToString();
+                nxtText.Content = current + 1 < totStudent ? studentNumList[current + 1].ToString() : "";
+                double goal = totStudent > 0 ? (double)(current + 1) / totStudent * 100 : 0;
+                ProgressBar.Value = goal;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,23 @@
+# Randotron
+![Demo](Demo.png)
+## Info
+Built with WPF and some extensions for material designing.
+
+With terrible codes.
+## Name inspiration
+This project's name is from the famous TV series Rick and Morty.
+
+And I think this is a great name for your project, so if you want to name yout repository with it, feel free to contact with me and I will change my repository's name.
+## Feature
+- Generate random student's number each time with equal possibility.
+- Still developing.
+## Known Bugs
+- If you don't type in the number of students and just press  `Enter`, then the program will show sorted numbers from `1` to `1000`.
+- Only supports Chinese.
+## 命名灵感
+灵感来源于瑞克和莫蒂 `S4E3`。由于我认为自己的代码配不上这么优秀的名字，因此如果你想将自己的仓库命名为 `Randotron`，请联系我。
+## 特点
+- 等概率地生成学生学号。
+- 更新中。
+## 已知问题
+- 如果错误地操作导致没有输入学生数量就点按了 `Enter`, 那么程序会依次输出 `1` 到 `1000` 的数字。
