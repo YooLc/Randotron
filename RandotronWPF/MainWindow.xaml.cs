bcf625b58d962084f880861529b8b6cd250ca357
YooLc <dlulc@sina.com>
2020-03-20 18:29:00 +0800

    Update and Fix 2020.03.20

--- RandotronWPF/MainWindow.xaml.cs
+++ RandotronWPF/MainWindow.xaml.cs
@@ -16,22 +16,63 @@ using System.Threading;
 
 namespace RandotronWPF
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window
     {
-        const int MAXS = 1000;
-        private int totStudent = 0, current = 0;
-        private int[] studentNumList = new int[MAXS];
-        private int number;
-
+        private const int MAXS = 1000;
+        private int StudentCount = 0, CurrentStudent = 0;
+        private int[] StudentNumList = new int[MAXS];
+        private bool StuNumConfirmed = false;
+        
         public MainWindow()
         {
             InitializeComponent();
+            ModeSelector.SelectedIndex = 0; // Set default mode
+            InitAll();
+            EnableMode();
             ClearAndShow();
         }
-        public int GetStudentNumber(TextBox textBox)
+        // Tools
+        private void InitAll()
+        {
+            StuNumConfirmed = false;
+            StuNumPanel.Visibility = Visibility.Hidden;
+            CurrentStudent = 0;
+            ProgressBar.Value = 0;
+        }
+
+        private void EnableMode()
+        {
+            if (ModeSelector.SelectedIndex == 0) StuNumPanel.Visibility = Visibility.Visible;
+        }
+
+        private void Random_Shuffle()
+        {
+            Random Randotron = new Random();
+            for (int i = StudentCount - 1; i >= 0; i--)
+            {
+                int CurrentStudentIndex = Randotron.Next(i + 1);
+                int TempValue = StudentNumList[CurrentStudentIndex];
+                StudentNumList[CurrentStudentIndex] = StudentNumList[i];
+                StudentNumList[i] = TempValue;
+            }
+        }
+
+        private void ClearAndShow()
+        {
+            for (int i = 0; i < MAXS; ++i) StudentNumList[i] = i + 1;
+            Random_Shuffle();
+            CurrentStudent = 0;
+            ProgressBar.Value = 0;
+        }
+
+        private void UpdateText(int Cur)
+        {
+            PreText.Content = Cur > 0 ? StudentNumList[Cur - 1].ToString() : "";
+            CurText.Content = StudentNumList[Cur].ToString();
+            NxtText.Content = Cur + 1 < StudentCount ? StudentNumList[Cur + 1].ToString() : "";
+        }
+
+        private int GetStudentNumber(TextBox textBox)
         {
             string content = textBox.Text;
             int t = 0;
@@ -43,59 +84,44 @@ namespace RandotronWPF
             }
             return t;
         }
-        private void SnackbarConfirmed(object sender, RoutedEventArgs e)
-        {
-            SnackbarTwo.IsActive = false;
-        }
+
+        private void PopUpBarConfirmed(object sender, RoutedEventArgs e) { PopUpBar.IsActive = false; }
         private void PopUp(string Message)
         {
-            SnackbarTwoMessage.Content = Message;
-            SnackbarTwo.IsActive = true;
+            PopUpBarMessage.Content = Message;
+            PopUpBar.IsActive = true;
         }
+        
+        private void ModeSelectorChange(object sender, RoutedEventArgs e)
+        {
+            InitAll();
+            EnableMode();
+        }
+
+        // Functions
         private void NumberModeConfirmed(object sender, RoutedEventArgs e)
         {
-            number = GetStudentNumber(numberTextBox);
-            if (number <= 0) { PopUp("请输入正确的学生人数，范围为[1, 1000]"); return; }
-            if (number > MAXS) { PopUp("最多仅允许 1000 个学生"); return; }
-            numberTextBox.Text = number.ToString();
-            totStudent = number;
+            int Tnumber = GetStudentNumber(NumberTextBox);
+            if (Tnumber <= 0) { PopUp("请输入正确的学生人数，范围为[1, 1000]"); return; }
+            if (Tnumber > MAXS) { PopUp("最多仅允许 1000 个学生"); return; }
+            NumberTextBox.Text = Tnumber.ToString();
+            StudentCount = Tnumber;
             ClearAndShow();
-            preText.Content = current > 0 ? studentNumList[current - 1].ToString() : "";
-            curText.Content = studentNumList[current].ToString();
-            nxtText.Content = current + 1 < totStudent ? studentNumList[current + 1].ToString() : "";
-            virtualTextBox.Focus();
+            UpdateText(CurrentStudent);
+            StuNumConfirmed = true;
+            FocusBox.Focus();
         }
-        private void random_Shuffle()
-        {
-            int currentIndex;
-            int tempValue;
-            Random r = new Random();
-            for (int i = totStudent - 1; i >= 0; i--)
-            {
-                currentIndex = r.Next(i + 1);
-                tempValue = studentNumList[currentIndex];
-                studentNumList[currentIndex] = studentNumList[i];
-                studentNumList[i] = tempValue;
-            }
-        }
-        private void ClearAndShow()
-        {
-            ModeSelect.SelectedIndex = 0;
-            for (int i = 0; i < MAXS; ++i) studentNumList[i] = i + 1;
-            random_Shuffle();
-            current = 0;
-            ProgressBar.Value = 0;
-        }
+
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Key == Key.Enter && number <= 1000 && number > 0)
+            if (ModeSelector.SelectedIndex == 0 && StuNumConfirmed == false) return;
+            if (ModeSelector.SelectedIndex != 0) { PopUp("目前仅支持学号模式"); return;}
+            if (e.Key == Key.Enter)
             {
-                current += 1;
-                if (current == totStudent) ClearAndShow();
-                preText.Content = current > 0 ? studentNumList[current - 1].ToString() : "";
-                curText.Content = studentNumList[current].ToString();
-                nxtText.Content = current + 1 < totStudent ? studentNumList[current + 1].ToString() : "";
-                double goal = totStudent > 0 ? (double)(current + 1) / totStudent * 100 : 0;
+                CurrentStudent += 1;
+                if (CurrentStudent == StudentCount) ClearAndShow();
+                UpdateText(CurrentStudent);
+                double goal = StudentCount > 0 ? (double)(CurrentStudent + 1) / StudentCount * 100 : 0;
                 ProgressBar.Value = goal;
             }
         }
